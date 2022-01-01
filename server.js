c76e2b88496cc3ed162f7fd9174809b64d3ebc38
robwhite <robert.white3120@gmail.com>
2022-01-01 13:16:54 -0600

    improved file read and write functionality by making it synchronous

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const fsPromises = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
 const requests = require('simple-requests');
 const JSSoup = require('jssoup').default;
 const { TwitterApi } = require('twitter-api-v2');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const app = express();
-dotenv.config()
+dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
 const client = new TwitterApi({
@@ -37,61 +39,121 @@ const rtSystemConditions = {
 
 const getRTData = () => {
      requests.get("https://www.ercot.com/content/cdr/html/real_time_system_conditions.html")
-             .then((response) => {
-               
-                    let soup = new JSSoup(response.data);      
-                    let currentData = soup.findAll('td', 'labelClassCenter');
-                    let lastUpdate = soup.find('div', 'schedTime');
-                    let objKeys = Object.keys(rtSystemConditions);
-
-                    for (let i = 0; i < currentData.length; i++){
-                         rtSystemConditions[objKeys[i]] = parseFloat(currentData[i].text);
-                    }
-                    rtSystemConditions["Last Update"] = lastUpdate.text.slice(14, lastUpdate.text.length);
-             })
-             .catch((err) => {
-                  res.send("error" + err);
-             })
+     .then((response) => {
+          
+          let soup = new JSSoup(response.data);      
+          let currentData = soup.findAll('td', 'labelClassCenter');
+          let lastUpdate = soup.find('div', 'schedTime');
+          let objKeys = Object.keys(rtSystemConditions);
+          
+          for (let i = 0; i < currentData.length; i++){
+               rtSystemConditions[objKeys[i]] = parseFloat(currentData[i].text);
+          }
+          rtSystemConditions["Last Update"] = lastUpdate.text.slice(14, lastUpdate.text.length);
+     })
+     .catch((err) => {
+          res.send("error" + err);
+     })
 }
+
+///////////////////////////////
+///////// LOG DATA ////////////
+///////////////////////////////
 
 const logRTData = () => {
      fs.mkdir(path.join(__dirname, 'logs'), (err) => {
           if(err.errno !== -17 ){
                console.log(err)
           }
-
+          
      })
-
+     
      let date = new Date().toLocaleDateString('en-US').replace(/\//g, '-'); // replaces all '/' with '-'
      console.log(date + " " + new Date().toLocaleTimeString('en-US'));
-     const stream = fs.createWriteStream(path.join(__dirname + "/logs/", date + '.txt'), {flags: 'a'});
-     for(props in rtSystemConditions){
-          stream.write(`${props}: ${rtSystemConditions[props]}` + "\n");
+
+     for (const key in rtSystemConditions){
+          fs.appendFileSync(path.join(__dirname, `/logs/${date}.txt`), (`${key}: ${rtSystemConditions[key]}` + '\n'));
      }
-     stream.end();
+}
+
+///////////////////////////////
+///////   PARSE DATA  /////////
+///////////////////////////////
+
+const parseRTData = (stat) => {
+     let sum = 0;
+
+     let date = new Date().toLocaleDateString('en-US').replace(/\//g, '-'); // replaces all '/' with '-';
+     
+     if (stat === 'avg-freq'){
+          let file = fs.readFileSync(path.join(__dirname, `/logs/${date}.txt`), 'utf-8').split('\n');
+          file.pop();
+          for (let i = 0; i < file.length; i += 14){
+               sum += parseFloat(file[i].slice(file[i].indexOf(':') + 2, file[i].length));
+          }
+          return (sum / file.length * 14).toFixed(3);
+     }
      
 }
 
+const checkGridStatus = (checkType=0) => {
+     // checkType 0 = auto
+     // checkType 1 = manual
+     
+     //server time is one hour ahead
+     
+     let serverHours = new Date().getHours();
+     let serverMinutes = new Date().getMinutes();
+     
+     if (checkType === 0){
+          if ( (serverHours === 11 && serverMinutes === 59) || (serverHours === 23 && serverMinutes === 59) ) {
+               console.log(parseRTData('avg-freq'));
+          }
+     }
+
+     if(checkType === 1){
+          return parseRTData('avg-freq');
+     }
+     
+     
+     
+}
 
 // ROUTES
 
 app.get("/ercot-api/", (req, res) => {
-     res.send("This is a test.");
-
+     res.send("ERCOT Realtime Grid Status API");
+     
 });
 
 app.get("/ercot-api/realtime", (req, res) => {
-     res.json(rtSystemConditions);
+     
+     new Promise((resolve, reject) => {
+          resolve(rtSystemConditions)
+          reject('no data')
+     }).then((data) => {
+          res.json(data);
+     }).catch((err) => {
+          res.send(err)
+     });
+
 });
 
+app.get("/ercot-api/gridstatus", (req, res) => {
+     res.send('average grid frequency for today: ' + checkGridStatus(1));
+})
+
 server = app.listen(PORT, () => {
-   console.log("Listening on port: " + PORT);  
-   
-   getRTData();
-   setInterval(() => {
-        logRTData();
-     //    client.v2.tweet('Current System Frequency: ' + rtSystemConditions['Current Frequency']);
-        console.log('logged');
-        getRTData();
-   }, 60000);
+     console.log("Listening on port: " + PORT);  
+     
+     getRTData();
+     
+     checkGridStatus();
+     
+     setInterval(() => {
+          logRTData();
+          //    client.v2.tweet('Current System Frequency: ' + rtSystemConditions['Current Frequency']);
+          console.log('logged');
+          getRTData();
+     }, 60000); // every 60 seconds data is logged
 });
